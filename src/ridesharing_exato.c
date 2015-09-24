07c4c98e29006cf99ea7831ac1229459b4e3041b
Arthur <arthur.jmacedo@gmail.com>
2015-09-24 09:06:30 -0300

    projeto inicial

---
+++ src/ridesharing_exato.c
@@ -0,0 +1,29 @@
+/**
+ * Aqui estou eu tentando
+ * criar um algoritmo exato para o problema multiobjetivo do ridehsaring
+ * chamado de Ridematching problm
+ */
+
+#include <stdio.h>
+
+/** data */
+int drivers_offers;
+int riders_requests;
+int trip_source[drivers_offers + riders_requests];
+int trip_destination[drivers_offers + riders_requests];
+float earlier_departure_time[drivers_offers + riders_requests];
+float latest_arrival_time[drivers_offers + riders_requests];
+float drivers_maximum_travel_time[drivers_offers];
+float drivers_maximum_travel_distance[drivers_offers];
+float riders_maximum_travel_time[riders_requests];
+
+
+
+
+int main(){
+
+	printf("Iniciando a aplicação");
+
+
+	return 0;
+}
